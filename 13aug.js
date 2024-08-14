a559ea7ac91afb89516feaeea89f2a395ffc477b
bisw235 <biswanathpanda562@gmail.com>
2024-08-14 22:07:49 +0530

    Add files via upload

---
+++ 13aug.js
@@ -0,0 +1,17 @@
+
+let val=document.querySelector(".fir").value;
+let butt=document.querySelector(".sec");
+ let vall=val;
+  butt.addEventListener("click",()=>{
+    if (vall>0){
+        alert("even");
+        
+    }
+     else{
+         alert("odd");
+     }
+  })
+
+  
+
+  
